877bd1480c3972bd8c5b25bbf0379c1e4ee42d03
Oleksandr Khomiak <khomiak@gmail.com>
2023-05-15 11:43:09 +0300

    some edits about pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 // підключаємо бібліотеку axios
 import axios from 'axios';
 
+// підключаємо бібліотеку react-toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 // підключення компонентів
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
@@ -24,15 +28,25 @@ export class App extends Component {
     query: '',
     showModal: false,
     imageLink: '',
-    // page: 1,
+    page: 1,
+    showBtnLoadMore: false,
   };
+
+  //  поки не розумію, що сюжи питсати
+  componentDidMount() {}
+  componentDidUpdate(prevProps, prevState) {}
 
   // що робити з отриманими даними при натиску кнопки у формі
   // записати значення тут в state App в поле query
   // викликати ф-ію, яка змінить imagesArray
   onSubmitSearchBtn = toFind => {
-    this.setState({ query: toFind });
+    this.setState({ query: toFind, imagesArray: [], page: 1 });
     this.getFromAPI(toFind, 1);
+  };
+
+  loadMorePictures = () => {
+    this.getFromAPI(this.state.query, this.state.page + 1);
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   // асинхронний метод для результатів запиту і що робити з ними
@@ -44,7 +58,7 @@ export class App extends Component {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: 12,
+      per_page: 4,
     });
 
     const URL = `${BASE_URL}?key=${API_KEY}&q=${toFind}&page=${page}&${searchParams}`;
@@ -53,13 +67,39 @@ export class App extends Component {
       const response = await axios.get(URL);
       console.log('response', response);
       console.log('response.data.total', response.data.total);
-      //якщо немає жодного збігу, то виводимо про це повідомлення
-      if (response.data.total < 1) {
-        console.log('результатів нема');
+      //
+      // якщо немає жодного збігу, то виводимо про це повідомлення
+      // і скидаємо стейт, query, щоб не засмічувався
+      if (response.data.totalHits < 1) {
+        toast('результатів нема!');
+        this.setState({ query: '' });
       }
-
-      if (response.data.total > 1) {
-        this.setState({ imagesArray: response.data.hits });
+      //
+      //Якщо у нас є результати для показу, то треба
+      // записати (розпилити) в стейт imagesArray отриманий масив
+      //
+      // якщо це перша сторінка, але є ще результати, то треба
+      // записати (розрилити) в стейт imagesArray отриманий масив
+      // і дізнатися чи є ще в базі картинки для показу, якщо так,
+      // то збільшити на 1 сторінку і показати кнопку  завантажити ще
+      else if (response.data.hits.length !== 0) {
+        this.setState(prevState => ({
+          imagesArray: [...prevState.imagesArray, ...response.data.hits],
+        }));
+        //
+        //рахуємо чи є ще на сервері фотографії.
+        //тобто чи треба показувати кнопку "завантажити ще" (статус showBtnLoadMore)
+        // для цього кількість результатів у одному запиті множимо на поточний номер сторінки
+        // і отримуємо кількість уже отриманих на компі картинок
+        // на сервері лишаються ще картинки, якщо ця цифра менше за response.data.totalHits
+        const alreadyDownloaded = 4 * this.state.page;
+        if (alreadyDownloaded < response.data.totalHits) {
+          toast('Натисни "завантажити ще", щоб отримати більше результатів!');
+          this.setState({ showBtnLoadMore: true });
+        } else {
+          toast('Це всі результати. Більше за цим запитом результатів нема!');
+          this.setState({ showBtnLoadMore: false });
+        }
       }
 
       //
@@ -86,14 +126,14 @@ export class App extends Component {
   //   // console.log('result', result);
   // }
 
-  // перемикання модалки показ чи ні
+  // перемикання модалки показ чи ні, вміст модалки передаємо пропсом зі стейт
   togleModal = () => {
     this.setState(prevState => ({
       showModal: !prevState.showModal,
     }));
   };
 
-  // лінк на фото для модального вікна
+  // встанвлення лінку фото для модального вікна (використовується в ImageGaleryItem)
   setImageLink = link => {
     this.setState({ imageLink: link });
   };
@@ -101,6 +141,7 @@ export class App extends Component {
   render() {
     return (
       <AppWrap>
+        <ToastContainer autoClose={2000} />
         <Searchbar onSubmit={this.onSubmitSearchBtn} />
         {this.state.query && (
           <ImageGallery
@@ -108,6 +149,9 @@ export class App extends Component {
             togleModal={this.togleModal}
             setImageLink={this.setImageLink}
           />
+        )}
+        {this.state.showBtnLoadMore && (
+          <button onClick={this.loadMorePictures}>Load more</button>
         )}
         {this.state.showModal && (
           <Modal
@@ -119,7 +163,3 @@ export class App extends Component {
     );
   }
 }
-
-//     //   {this.state.imagesArray.length > 0 && (
-//     //     <button onClick={this.loadMorePictures}>load more</button>
-//     //   )}
